d93adce47dcc78d43bf7798e2fc61d89f05e1716
Caitriona Hastings <ch2101@cam.ac.uk>
2026-02-05 15:48:42 +0000

    trying to fix the install problems

--- src/NanoSC_project/model.py
+++ src/NanoSC_project/model.py
@@ -24,7 +24,7 @@ class TwoLayerCNN(nn.Module):
 
         # Layer 4: Fully Connected
         self.out = nn.Linear(64, 10) # Output 10 values
-        self.sigmoid = nn.Sigmoid() # Squash to 0-1
+        self.softmax = nn.Softmax() # Squash to 0-1
         
     def forward(self, x):
         # Layer 1
@@ -40,6 +40,6 @@ class TwoLayerCNN(nn.Module):
         # Dense Layers
         x = self.relu(self.fc1(x))
         x = self.drop(x)
-        x = self.sigmoid(self.out(x))
+        x = self.softmax(self.out(x))
         return x
 
